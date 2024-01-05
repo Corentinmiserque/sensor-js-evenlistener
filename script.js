9f4106611640323bc1bdd7e2f9804a458608303d
Corentinmiserque <corentinmiserque1@gmail.com>
2024-01-05 09:52:23 +0100

    push

--- script.js
+++ script.js
@@ -2,33 +2,11 @@ const sections = document.querySelectorAll('.section');
 let currentSection = 0;
 let interval = true;
 
-function requestOrientationPermission() {
-    if ('DeviceOrientationEvent' in window) {
-        // Vérifie si la méthode requestPermission existe
-        if (DeviceOrientationEvent.requestPermission) {
-            DeviceOrientationEvent.requestPermission()
-                .then(response => {
-                    if (response == 'granted') {
-                        // L'autorisation a été accordée, ajoute l'événement 'deviceorientation'
-                        window.addEventListener('deviceorientation', handleOrientation);
-                    }
-                })
-                .catch(console.error);
-        } else {
-            // La méthode requestPermission n'existe pas, suppose que l'autorisation est accordée
-            // Ajoute l'événement 'deviceorientation'
-            window.addEventListener('deviceorientation', handleOrientation);
-        }
-    } else {
-        console.error("L'appareil ne prend pas en charge l'événement deviceorientation");
-    }
-}
-
-function handleOrientation(event) {
+window.addEventListener('deviceorientation', function(event) {
     const beta = event.beta;
 
     if (interval) {
-        interval = false;
+       interval = false;
 
         if (beta < 45 && currentSection < sections.length - 1) {
             // Inclinaison vers le haut
@@ -44,14 +22,6 @@ function handleOrientation(event) {
 
         setTimeout(() => {
             interval = true;
-        }, 1000);
+        }, 1000); 
     }
-}
-
-// Demande la permission d'orientation lors du chargement de la page (vous pouvez le déclencher à un autre moment)
-window.onload = function() {
-    const shouldRequestPermission = window.confirm("Voulez-vous autoriser l'accès à l'orientation de l'appareil?");
-    if (shouldRequestPermission) {
-        requestOrientationPermission();
-    }
-};
+}, false);
