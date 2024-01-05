b3811aa059a3579b9106a54f19b2c0a54c5b1edb
Corentinmiserque <corentinmiserque1@gmail.com>
2024-01-05 09:25:23 +0100

    test

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 const sections = document.querySelectorAll('.section');
 let currentSection = 0;
+let canPerformAction = true;
 
 window.addEventListener('deviceorientation', function(event) {
     const beta = event.beta;
 
-    if (beta < 45 && currentSection < sections.length - 1) {
-        // Inclinaison à haut
-        currentSection++;
-    } else if (beta > 135 && currentSection > 0) {
-        // Inclinaison en bas 
-        currentSection--;
+    if (canPerformAction) {
+        canPerformAction = false;
+
+        if (beta < 45 && currentSection < sections.length - 1) {
+            // Inclinaison vers le haut
+            currentSection++;
+        } else if (beta > 135 && currentSection > 0) {
+            // Inclinaison vers le bas
+            currentSection--;
+        }
+
+        sections.forEach((section, index) => {
+            section.classList.toggle('active', index === currentSection);
+        });
+
+        setTimeout(() => {
+            canPerformAction = true;
+        }, 500); // 0.5 seconde d'intervalle
     }
-
-    sections.forEach((section, index) => {
-        section.classList.toggle('active', index === currentSection);
-    });
 }, false);
-
