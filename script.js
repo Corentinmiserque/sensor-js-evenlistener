e3df64d1a17057d6946fd8e1d610855fa31a4096
Corentinmiserque <corentinmiserque1@gmail.com>
2024-01-05 09:40:30 +0100

    permsion

--- script.js
+++ script.js
@@ -2,11 +2,29 @@ const sections = document.querySelectorAll('.section');
 let currentSection = 0;
 let interval = true;
 
-window.addEventListener('deviceorientation', function(event) {
+// Vérifie si l'appareil prend en charge l'événement 'deviceorientation'
+if ('DeviceOrientationEvent' in window) {
+    // Demande la permission pour les événements 'deviceorientation' sur iOS
+    DeviceOrientationEvent.requestPermission()
+        .then(permissionState => {
+            if (permissionState === 'granted') {
+                // L'autorisation a été accordée, vous pouvez écouter l'événement 'deviceorientation'
+                window.addEventListener('deviceorientation', handleOrientation, false);
+            } else {
+                // L'utilisateur a refusé l'autorisation
+                console.error('Permission refusée pour les événements deviceorientation');
+            }
+        })
+        .catch(console.error);
+} else {
+    console.error("L'appareil ne prend pas en charge l'événement deviceorientation");
+}
+
+function handleOrientation(event) {
     const beta = event.beta;
 
     if (interval) {
-       interval = false;
+        interval = false;
 
         if (beta < 45 && currentSection < sections.length - 1) {
             // Inclinaison vers le haut
@@ -22,6 +40,6 @@ window.addEventListener('deviceorientation', function(event) {
 
         setTimeout(() => {
             interval = true;
-        }, 1000); 
+        }, 1000);
     }
-}, false);
+}
